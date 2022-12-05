8568850d9840b3682a34085180954a8b8f4f4ffb
zee <93104681+RezaAditiyaa@users.noreply.github.com>
2022-12-05 22:13:16 +0700

    Add files via upload

---
+++ Calculator-App-main/CalculatorApp/FrmHasil.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace CalculatorApp
+{
+    partial class FrmHasil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstHasil = new System.Windows.Forms.ListBox();
+            this.btnHitung = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lstHasil);
+            this.groupBox1.Location = new System.Drawing.Point(18, 22);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(388, 304);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "[ Hasil ]";
+            // 
+            // lstHasil
+            // 
+            this.lstHasil.FormattingEnabled = true;
+            this.lstHasil.Location = new System.Drawing.Point(11, 17);
+            this.lstHasil.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lstHasil.Name = "lstHasil";
+            this.lstHasil.Size = new System.Drawing.Size(374, 277);
+            this.lstHasil.TabIndex = 1;
+            this.lstHasil.SelectedIndexChanged += new System.EventHandler(this.lstHasil_SelectedIndexChanged);
+            // 
+            // btnHitung
+            // 
+            this.btnHitung.Location = new System.Drawing.Point(115, 331);
+            this.btnHitung.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnHitung.Name = "btnHitung";
+            this.btnHitung.Size = new System.Drawing.Size(186, 25);
+            this.btnHitung.TabIndex = 2;
+            this.btnHitung.Text = "Hitung";
+            this.btnHitung.UseVisualStyleBackColor = true;
+            this.btnHitung.Click += new System.EventHandler(this.btnHitung_Click);
+            // 
+            // FrmHasil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 366);
+            this.Controls.Add(this.btnHitung);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.Name = "FrmHasil";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hasil Perhitungan";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lstHasil;
+        private System.Windows.Forms.Button btnHitung;
+    }
+}
+
